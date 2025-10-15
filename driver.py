20a5222151391393033893e54c78236ee9a1cdae
RunoWazlib <ethanrog01@gmail.com>
2025-10-15 11:56:21 -0500

    Initial Python Management

---
+++ driver.py
@@ -0,0 +1,10 @@
+from src.core.static_info import StaticInfo
+
+# Generate StaticInfo instance
+StaticInfoInstance = StaticInfo()
+
+StaticInfoInstance.getHardwareStats()
+
+# # Assess current system operations while program is running
+# while True:
+#     pass
---
+++ src/core/static_info.py
@@ -0,0 +1,10 @@
+import cpp_bindings as c
+
+class StaticInfo:
+    def __init__(self):
+       # Determine OS name using the C++ function
+       self.osDict = {0:"Linux", 1:"MacOS", 2:"Windows"}
+       self.osName = self.osDict[c.getOSname_py()]
+    
+    def getHardwareStats(self):
+        self.hardware = c.getSystemInfoStatic_py()
